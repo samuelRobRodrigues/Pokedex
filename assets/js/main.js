51c53ea09c2b4660e91db637a6009980d33d0cbb
samuelRobRodrigues <samucada160@gmail.com>
2023-12-14 21:21:44 -0300

    listagem_pronta

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,34 @@ const limit = 10;
 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
+function pokemonConverter(pokemon) {
+    return `
+        <li class="pokemon">
+                
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+        <ol class="types">
+            <li class="type">grass</li>
+            <li class="type">poison</li>
+        </ol>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+            alt="${pokemon.name}">
+        </div>    
+        </li>
+        `}
+const pokemonList = document.getElementById('pokemonList')
+
 fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .then((pokemonList) => {
-        debugger
-        console.log(pokemonList)
+    .then((pokemons) => {
+
+        for (let i = 1; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            
+            pokemonList.innerHTML += pokemonConverter(pokemon)
+        }
+        
     })
     .catch((error) => console.log(error)) 
